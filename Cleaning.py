8bad420f4bc6c36fc7de91ca49b60a5c11e4e0a5
ABDURRAHMAN SALIH <92715976+salih-17@users.noreply.github.com>
2023-09-17 01:59:23 +0300

    Add models

---
+++ Cleaning.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug  8 19:22:19 2023
+
+@author: abdur
+"""
+
+import pandas as pd
+import re
+
+import nltk
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+STOPWORDS = set(stopwords.words('english'))
+
+# ['Category', 'Rating', 'Label', 'Text_Reviews']
+
+
+def clean_text(text):
+    
+    
+    pattern = re.compile('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+')
+    text = pattern.sub('', text)
+    text = " ".join(filter(lambda x:x[0]!='@', text.split()))
+    emoji = re.compile("["
+                           u"\U0001F600-\U0001FFFF"  # emoticons
+                           u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+                           u"\U0001F680-\U0001F6FF"  # transport & map symbols
+                           u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                           u"\U00002702-\U000027B0"
+                           u"\U000024C2-\U0001F251"
+                           "]+", flags=re.UNICODE)
+    
+    text = emoji.sub(r'', text)
+    text = text.lower()
+    text = re.sub(r"i'm", "i am", text)
+    text = re.sub(r"he's", "he is", text)
+    text = re.sub(r"she's", "she is", text)
+    text = re.sub(r"that's", "that is", text)        
+    text = re.sub(r"what's", "what is", text)
+    text = re.sub(r"where's", "where is", text) 
+    text = re.sub(r"\'ll", " will", text)  
+    text = re.sub(r"\'ve", " have", text)  
+    text = re.sub(r"\'re", " are", text)
+    text = re.sub(r"\'d", " would", text)
+    text = re.sub(r"\'ve", " have", text)
+    text = re.sub(r"won't", "will not", text)
+    text = re.sub(r"don't", "do not", text)
+    text = re.sub(r"did't", "did not", text)
+    text = re.sub(r"can't", "can not", text)
+    text = re.sub(r"it's", "it is", text)
+    text = re.sub(r"couldn't", "could not", text)
+    text = re.sub(r"have't", "have not", text)
+    text = re.sub(r"[,.\"\'!@#$%^&*(){}?/;`~:<>+=-]", "", text)
+    return text
+
+
+def clean (Text):
+    text = str(Text).lower()
+
+    # Remove StopWord
+    Cleaning1 = ' '.join([item for item in text.split() if item not in STOPWORDS])
+
+    CleanedText=clean_text (Cleaning1)
+    return CleanedText 
+
+
